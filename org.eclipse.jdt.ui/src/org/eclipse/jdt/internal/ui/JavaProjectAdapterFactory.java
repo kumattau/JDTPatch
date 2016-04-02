941d5efde8ec3d86e21cde1047524a9f97ddec9b
kumattau <kumattau@gmail.com>
2016-04-03 01:15:04 +0900

    Add feature patch and 4.5.2 plugins.

---
+++ org.eclipse.jdt.ui/src/org/eclipse/jdt/internal/ui/JavaProjectAdapterFactory.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.internal.ui;
+
+
+import org.eclipse.core.runtime.IAdapterFactory;
+
+import org.eclipse.core.resources.IProject;
+
+import org.eclipse.jdt.core.IJavaProject;
+
+/**
+ * An adapter factory for IJavaProjects.
+ */
+public class JavaProjectAdapterFactory implements IAdapterFactory {
+
+	private static Class<?>[] PROPERTIES= new Class[] {
+		IProject.class,
+	};
+
+	public Class<?>[] getAdapterList() {
+		return PROPERTIES;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T getAdapter(Object element, Class<T> key) {
+		if (IProject.class.equals(key)) {
+			IJavaProject javaProject= (IJavaProject)element;
+			return (T) javaProject.getProject();
+		}
+		return null;
+	}
+}
